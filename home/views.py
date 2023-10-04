478428a32ec478897996b45241f7dec28fedbf40
Aaryan Shah <aaryanshah615@gmail.com>
2023-10-04 21:53:14 +0545

    blog api fully responsive

--- home/views.py
+++ home/views.py
@@ -11,6 +11,15 @@ class home(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def get(self,request):
+        blog=Blog.objects.filter(user=request.user)
+        serializer=Blogserializer(blog,many=True)
+        return Response({
+                'data':serializer.data,
+                'msg':'data created by you'
+            })
+
+
     def post(self,request):
         data=request.data
         data['user']=request.user.id
@@ -26,6 +35,61 @@ class home(APIView):
                 'data':serializer.data,
                 'msg':'User created successfully'
             })
+    def patch(self,request):
+
+        data=request.data
+        blog=Blog.objects.filter(uid=data.get('uid'))
+
+        if not blog.exists():
+            return Response({
+                'data':'',
+                'msg':'Blog not found'
+            })
+        
+        if request.user != blog[0].user:
+            return Response({
+                'data':'',
+                'msg':'you are not authorized to changed'
+            })
+        
+        serializer=Blogserializer(blog[0],data=data,partial=True)
+        if not serializer.is_valid():
+         
+
+          return Response({
+                'data':serializer.data,
+                'msg':'Blog not updated successfully'
+            })
+        serializer.save()
+        return Response({
+                'data':serializer.data,
+                'msg':'Blog updated successfully'
+            })
+    
+
+    def delete(self,request):
+        data=request.data
+        blog=Blog.objects.filter(uid=data.get('uid'))
+
+        if not blog.exists():
+            return Response({
+                'data':'',
+                'msg':'Blog not found'
+            })
+        
+        if request.user != blog[0].user:
+            return Response({
+                'data':'',
+                'msg':'you are not authorized to changed'
+            })
+        
+        blog[0].delete()
+        return Response({
+                'data':'',
+                'msg':'data deleted successfully'
+            })
+
+
 
 
 
